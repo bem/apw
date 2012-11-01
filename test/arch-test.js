227f1f01b1922c528a4251d353823ca4f71d4b0f
Vladimir Alaev <scff@yandex-team.ru>
2012-11-01 20:16:20 +0200

    Lazy linking refactored
    Added removing of the lazy links in unlink()
    Added tests

--- test/arch-test.js
+++ test/arch-test.js
@@ -2,6 +2,8 @@ var APW = require(process.env.COVER? '../lib-cov/apw' : '../lib/apw'),
     ASSERT = require('assert'),
     COMMON = require('./common'),
 
+    extend = require('node.extend'),
+
     getSimpleArch = COMMON.getSimpleArch,
     getEmptyArch = COMMON.getEmptyArch,
     createNode = COMMON.createNode,
@@ -239,6 +241,48 @@ describe('Node link', function() {
         
         ASSERT.equal(parents.length, 1);
         ASSERT.equal(parents[0], 'A');
+    });
+});
+
+describe('Node lazy links', function() {
+    beforeEach(function() {
+        arch = getArch2();
+    });
+
+    it('link() E -> nonexistent creates lazy link', function() {
+        var c = extend({}, arch.children),
+            p = extend({}, arch.parents),
+            link = {
+                parent: 'X',
+                child: 'E',
+                ref: 0
+            };
+
+        arch.link('E', 'X', true);
+
+        ASSERT.deepEqual(arch.children, c);
+        ASSERT.deepEqual(arch.parents, p);
+        ASSERT.deepEqual(arch.lazyLinks,
+            {
+                E: [link],
+                X: [link]
+            }
+        );
+    });
+
+    it('lazy link becomes real when both linking nodes become available in arch', function() {
+        arch.link('E', 'X', true);
+        arch.addNode(createNode('X'));
+
+        ASSERT(arch.hasChildren('X', 'E'));
+    });
+
+    it('unlink() removes lazy link', function() {
+        arch.link('E', 'X', true);
+        arch.addNode(createNode('X'));
+        arch.unlink('E', 'X');
+
+        ASSERT.deepEqual(arch.lazyLinks, {});
     });
 });
 
